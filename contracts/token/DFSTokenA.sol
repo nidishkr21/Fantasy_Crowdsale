2e48a6132f7e20cae8227990858abe2be0defca7
Nidish Ramakrishnan <nramakrishnan@grepruby.com>
2017-11-30 18:49:52 +0530

    Added DFSTokenA for founder shares

---
+++ contracts/token/DFSTokenA.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.11;
+
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ * @dev Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ */
+contract DFSToken {
+
+  string public constant name = "DFS Token A";
+  string public constant symbol = "DFA";
+  uint8 public constant decimals = 18;
+  uint256 public constant INITIAL_SUPPLY = 140000000 * (10 ** uint256(decimals));
+  bool internal initalSupplySet = false;
+
+  /**
+   * @dev Constructor that gives msg.sender all of existing tokens.
+   */
+  function DFSTokenA()
+    DFSTokenAControl()
+  {
+    // initial token distribution to be put in here
+    require(initalSupplySet == false);
+    _setTotalSupplyDFA(INITIAL_SUPPLY);
+    _setDFABalanceOf(msg.sender, INITIAL_SUPPLY);
+    initalSupplySet = true;
+  }
+}
