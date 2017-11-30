7f0daf8bfb17310d3d9150fa9c9486b1a877e07d
Nidish Ramakrishnan <nramakrishnan@grepruby.com>
2017-11-30 18:54:45 +0530

    Changed cap details for crowdsale

---
+++ contracts/crowdsale/CappedCrowdsaleA.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.4.11;
+
+import './CrowdsaleA.sol';
+
+/**
+ * @title CappedCrowdsaleA
+ * @dev Extension of CrowdsaleA with a max amount of funds raised
+ */
+contract CappedCrowdsaleA is CrowdsaleA {
+
+  uint256[2] public softCap;
+  uint256[2] public totalSupply;
+
+
+  function CappedCrowdsaleA() {
+    softCap = [60000000e18, 200000000e18];
+  }
+
+
+  function withSoftCap() internal returns (bool) {
+    if (now < startTime + preSaleTime) {
+      return totalSupply[0] <= softCap[0];
+    }
+    return totalSupply[1] <= softCap[1];
+  }
+
+
+  function recordSupply(uint256 tokens) internal returns (uint256) {
+    if (now < startTime + preSaleTime) {
+       totalSupply[0] = totalSupply[0].add(tokens);
+       return;
+    }
+    totalSupply[1] = totalSupply[1].add(tokens);
+  }
+
+
+  function softCapReached(uint256 phase) internal returns (bool) {
+    return totalSupply[phase] >= softCap[phase];
+  }
+
+  // overriding CrowdsaleA#validPurchase to add extra hardCap logic
+  // @return true if investors can buy at the moment
+  function validPurchase() internal constant returns (bool) {
+    return super.validPurchase() && withSoftCap();
+  }
+
+  // overriding CrowdsaleA#hasEnded to add hardCap logic
+  // @return true if CrowdsaleA event has ended
+  function saleHasEnded() public constant returns (bool) {
+    return super.saleHasEnded() || softCapReached(1);
+  }
+
+  // @return true if presale event has ended
+  function preSaleHasEnded() public constant returns (bool) {
+    return now >= (startTime + 7 days) || softCapReached(0);
+  }
+
+}
