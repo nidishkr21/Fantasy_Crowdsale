e8f5e37be8a85223ac958faea100aa478a2ae6d8
Nidish Ramakrishnan <nramakrishnan@grepruby.com>
2017-12-08 20:36:25 +0530

    Test case on future crowdsales

--- test/crowdsale.js
+++ test/crowdsale.js
@@ -1,11 +1,14 @@
+const MockDFSACrowdsale = artifacts.require('./helpers/MockDFSACrowdsale.sol');
 const DFSACrowdsale = artifacts.require('./crowdsale/A/DFSACrowdsale.sol');
 const DFSTokenA = artifacts.require('./token/A/DFSTokenA.sol');
+const MockDFSBCrowdsale = artifacts.require('./helpers/MockDFSBCrowdsale.sol');
 const DFSBCrowdsale = artifacts.require('./crowdsale/B/DFSBCrowdsale.sol');
 const DFSTokenB = artifacts.require('./token/B/DFSTokenB.sol');
 const DataCentre = artifacts.require('./token/DataCentre.sol');
 const MultisigWallet = artifacts.require('./multisig/solidity/MultiSigWalletWithDailyLimit.sol');
 import {advanceBlock} from './helpers/advanceToBlock';
 import latestTime from './helpers/latestTime';
+import increaseTime from './helpers/increaseTime';
 const BigNumber = require('bignumber.js');
 const assertJump = require('./helpers/assertJump');
 const ONE_ETH = web3.toWei(1, 'ether');
@@ -152,6 +155,65 @@ contract('Crowdsale', (accounts) => {
     });
   })
 
+  describe('#future crowdsalesA', () => {
+    let tokenA;
+    let dfsACrowdsale;
+
+    beforeEach(async () => {
+      await advanceBlock();
+      const startTime = latestTime();
+      tokenA = await DFSTokenA.new();
+      dfsACrowdsale = await MockDFSACrowdsale.new(startTime, PRE_SALE_DAYS, tokenA.address, multisigWallet.address);
+      await tokenA.transferOwnership(dfsACrowdsale.address);
+      await dfsACrowdsale.unpause();
+    });
+
+    it('should allow to start another crowdsale manually after succesful completetion of the first one', async () => {
+
+      const amountEth = [new BigNumber(15000).mul(MOCK_ONE_ETH), new BigNumber(62500).mul(MOCK_ONE_ETH)];
+
+      const INVESTORS = accounts[4];
+      await dfsACrowdsale.buyTokens(INVESTORS, {value: amountEth[0], from: INVESTORS});
+
+      // forward time by PRE_SALE_DAYS days
+      await increaseTime(86400 * PRE_SALE_DAYS);
+      await dfsACrowdsale.buyTokens(INVESTORS, {value: amountEth[1], from: INVESTORS});
+
+
+      // check succesful completetion
+      const totalSupply = [(await dfsACrowdsale.totalSupply.call(0)).toNumber(), (await dfsACrowdsale.totalSupply.call(1)).toNumber()];
+      const softCap = [(await dfsACrowdsale.softCap.call(0)).toNumber(), (await dfsACrowdsale.softCap.call(1)).toNumber()];
+      const initialSupply = await tokenA.INITIAL_SUPPLY.call();
+      const tokenTotalSupply = await tokenA.totalSupply.call();
+
+      assert.equal(totalSupply[0], softCap[0]);
+      assert.equal(totalSupply[1], softCap[1]);
+      assert.equal(initialSupply.add(totalSupply[0]).add(totalSupply[1]).toNumber(), tokenTotalSupply.toNumber());
+
+
+      // finish the first crowdsale and transfer ownership of token to escrowCouncil
+      await dfsACrowdsale.pause();
+      await dfsACrowdsale.transferTokenOwnership(accounts[0]);
+      await tokenA.finishMinting();
+
+      // deploy new crowdsale contract
+      const startTime1 = latestTime();
+
+      const dfsACrowdsaleNew = await MockDFSACrowdsale.new(startTime1, PRE_SALE_DAYS, tokenA.address, multisigWallet.address);
+      await tokenA.startMinting();
+      await tokenA.transferOwnership(dfsACrowdsaleNew.address);
+      await dfsACrowdsaleNew.unpause();
+
+
+      // check the buy function and balance
+      const NEW_INVESTOR = accounts[5];
+      await dfsACrowdsaleNew.buyTokens(NEW_INVESTOR, {value: MOCK_ONE_ETH, from: INVESTORS});
+
+      const balance = await tokenA.balanceOf.call(NEW_INVESTOR);
+      assert.equal(balance.toNumber(), MOCK_ONE_ETH*4000);
+    });
+  });
+
   describe('#CrowdsaleB security considerations', () => {
     let tokenB;
     let dfsBCrowdsale;
@@ -217,4 +279,62 @@ contract('Crowdsale', (accounts) => {
       }
     });
   })
+  
+  describe('#future crowdsalesB', () => {
+    let tokenB;
+    let dfsBCrowdsale;
+
+    beforeEach(async () => {
+      await advanceBlock();
+      const startTime = latestTime();
+      tokenB = await DFSTokenB.new();
+      dfsBCrowdsale = await MockDFSBCrowdsale.new(startTime, PRE_SALE_DAYS, tokenB.address, multisigWallet.address);
+      await tokenB.transferOwnership(dfsBCrowdsale.address);
+      await dfsBCrowdsale.unpause();
+    });
+
+    it('should allow to start another crowdsale manually after succesful completetion of the first one', async () => {
+
+      const amountEth = [new BigNumber(37500).mul(MOCK_ONE_ETH), new BigNumber(62500).mul(MOCK_ONE_ETH)];
+
+      const INVESTORS = accounts[4];
+      await dfsBCrowdsale.buyTokens(INVESTORS, {value: amountEth[0], from: INVESTORS});
+
+      // forward time by PRE_SALE_DAYS days
+      await increaseTime(86400 * PRE_SALE_DAYS);
+      await dfsBCrowdsale.buyTokens(INVESTORS, {value: amountEth[1], from: INVESTORS});
+
+      // check succesful completetion
+      const totalSupply = [(await dfsBCrowdsale.totalSupply.call(0)).toNumber(), (await dfsBCrowdsale.totalSupply.call(1)).toNumber()];
+      const softCap = [(await dfsBCrowdsale.softCap.call(0)).toNumber(), (await dfsBCrowdsale.softCap.call(1)).toNumber()];
+      const initialSupply = await tokenB.INITIAL_SUPPLY.call();
+      const tokenTotalSupply = await tokenB.totalSupply.call();
+
+      assert.equal(totalSupply[0], softCap[0]);
+      assert.equal(totalSupply[1], softCap[1]);
+      assert.equal(initialSupply.add(totalSupply[0]).add(totalSupply[1]).toNumber(), tokenTotalSupply.toNumber());
+
+
+      // finish the first crowdsale and transfer ownership of token to escrowCouncil
+      await dfsBCrowdsale.pause();
+      await dfsBCrowdsale.transferTokenOwnership(accounts[0]);
+      await tokenB.finishMinting();
+
+      // deploy new crowdsale contract
+      const startTime1 = latestTime();
+
+      const dfsBCrowdsaleNew = await MockDFSBCrowdsale.new(startTime1, PRE_SALE_DAYS, tokenB.address, multisigWallet.address);
+      await tokenB.startMinting();
+      await tokenB.transferOwnership(dfsBCrowdsaleNew.address);
+      await dfsBCrowdsaleNew.unpause();
+
+
+      // check the buy function and balance
+      const NEW_INVESTOR = accounts[5];
+      await dfsBCrowdsaleNew.buyTokens(NEW_INVESTOR, {value: MOCK_ONE_ETH, from: INVESTORS});
+
+      const balance = await tokenB.balanceOf.call(NEW_INVESTOR);
+      assert.equal(balance.toNumber(), MOCK_ONE_ETH*800);
+    });
+  });
 });
